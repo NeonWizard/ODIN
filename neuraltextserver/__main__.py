e8de4e4b632c876819b0ba904b92bef9797ab9a0
Spooky <averagewizard13@gmail.com>
2021-04-25 19:12:22 -0600

    Basic functioning REST server

--- neuraltextserver/__main__.py
+++ neuraltextserver/__main__.py
@@ -1,7 +1,33 @@
-class Server:
-	pass
+import flask
+from flask import request, jsonify
+import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True if "DEBUG" in os.environ and os.environ["DEBUG"] == "true" else False
+
+@app.route("/api/neuraltext/ping", methods=["GET"])
+def ping(): return "pong"
+
+@app.route("/api/neuraltext", methods=["GET"])
+def homepage():
+	routes = []
+
+	for rule in app.url_map.iter_rules():
+		if not str(rule).startswith("/api"): continue
+		routes.append('%s' % rule)
+
+	return jsonify(routes)
+
+@app.route("/api/neuraltext/model", methods=["GET"])
+def generate():
+	model = request.args.get("model")
+	length = request.args.get("length")
+
+	print(model, length)
 
 if __name__ == "__main__":
 	# start server
-	pass
+	app.run()
