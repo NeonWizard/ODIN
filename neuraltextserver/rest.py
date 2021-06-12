acde9f80c90332754a42a5cad2326f164a57c0c1
Wes <averagewizard13@gmail.com>
2021-06-12 16:35:15 -0600

    Change API docs to be Flask template

--- neuraltextserver/rest.py
+++ neuraltextserver/rest.py
@@ -13,7 +13,55 @@ def docs():
 	API documentation.
 	"""
 
-	return None
+	# TODO: move to external file and extrapolate from methods
+	endpoints = [
+		{
+			"method": "GET",
+			"path": "/ping",
+			"description": "Returns 200 OK and the text \"pong\" to signal the server is online.",
+			"params": [],
+			"response": {
+				"data": "ping"
+			}
+		},
+		{
+			"method": "GET",
+			"path": "/models",
+			"description": "Returns all available GPT-2 models by name.",
+			"params": [],
+			"response": {
+				"data": [
+					{
+					"id": 0,
+					"name": "john-keats-300"
+					},
+					{
+					"id": 1,
+					"name": "deiga-500"
+					}
+				],
+				"meta": {
+					"result_count": 2
+				}
+			}
+		},
+		{
+			"method": "GET",
+			"path": "/models/<name>",
+			"description": "Generates text via a specified GPT-2 model.",
+			"params": [
+				["words", "int", "Number of words to generate."]
+			],
+			"response": {
+				"data": "Howdy, my name is Rawhide Kobayashi. I'm a 27 year old Japanese Japamerican (western culture fan for you foreigners). I brand and wrangle cattle on my ranch, and spend my days perfecting the craft and enjoying superior American passtimes. (Barbeque, Rodeo, Fireworks) I train with my branding iron every day.",
+				"meta": {
+					"generation_time": "15582"
+				}
+			}
+		},
+	]
+
+	return flask.render_template("api_documentation.html", endpoints=endpoints)
 
 @app.route("/api/models", methods=["GET"])
 def models():
@@ -21,7 +69,7 @@ def models():
 	Returns all available GPT-2 models by name.
 	"""
 
-	return flask.render_template("")
+	return []
 
 @app.route("/api/models/<name>", methods=["GET"])
 def generate(name):
@@ -32,6 +80,8 @@ def generate(name):
 	length = request.args.get("length")
 
 	print(name, length)
+
+	return {}
 
 
 if __name__ == "__main__":
