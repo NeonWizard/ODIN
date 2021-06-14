ce920eefeee53733b05e000ba4dac4fcc7b2daa2
Wes <averagewizard13@gmail.com>
2021-06-14 14:08:52 -0600

    Add arguments to generate endpoint

--- api/app.py
+++ api/app.py
@@ -40,8 +40,40 @@ def generate(name):
 	# Two separate endpoints: request_generate and generate. The first would return a token from the server, which would have to be sent
 	# in the call to the second endpoint.
 
-	# length = request.args.get("length")
+	# - Parse arguments
+	length = request.form.get("length", 512, type=int)
+	truncate = request.form.get("truncate", None, type=str) # token to truncate at
+	prefix = request.form.get("prefix", None, type=str)
+	seed = request.form.get("seed", None, type=int)
+	temperature = request.form.get("temperature", 0.7, type=float)
+	top_k = request.form.get("top_k", 0, type=int)
+	top_p = request.form.get("top_p", 0.0, type=float)
 
+	include_prefix = request.form.get("include_prefix", "true", type=str)
+	include_prefix = include_prefix.lower() == "true"
+
+	sample_delimiter = request.form.get("sample_delimiter", "=" * 20 + "\n", type=str)
+	n_samples = request.form.get("n_samples", 1, type=int)
+	if n_samples == 1: sample_delimiter = ""
+	batch_size = request.form.get("batch_size", 1, type=int) # Number of batches (only affects speed/memory. Must divide n_samples
+
+	# - Validate arguments
+	if length <= 0 or length >= 16384:
+		return { "error": "Valid length is between 0 and 16384 exclusively." }, 400
+	if temperature < 0.0 or temperature > 1.0:
+		return { "error": "Valid temperature is between 0.0 and 1.0 inclusively." }, 400
+	if top_k != 0 and top_p != 0.0:
+		return { "error": "top_k and top_p are mutually exclusive." }, 400
+	if top_k < 0:
+		return { "error": "top_k must be a non-negative number." }, 400
+	if top_p < 0.0 or top_p > 1.0:
+		return { "error": "Valid top_p is between 0.0 and 1.0 inclusively." }, 400
+	if n_samples <= 0 or n_samples > 8:
+		return { "error": "Valid n_samples is between 0 and 8 inclusively." }, 400
+	if n_samples % batch_size != 0:
+		return { "error": "Batch sizes must be able to divide n_samples" }, 400
+
+	# - Generate response
 	start_time = datetime.datetime.now()
 
 	result = odin.generate(name)
@@ -51,7 +83,20 @@ def generate(name):
 	return {
 		"data": result,
 		"meta": {
-			"generation_time": round(gen_time.total_seconds() * 1000)
+			"generation_time": round(gen_time.total_seconds() * 1000),
+			"request": {
+				"length": length,
+				"truncate": truncate,
+				"prefix": prefix,
+				"seed": seed,
+				"temperature": temperature,
+				"top_k": top_k,
+				"top_p": top_p,
+				"include_prefix": include_prefix,
+				"sample_delimiter": sample_delimiter,
+				"n_samples": n_samples,
+				"batch_size": batch_size
+			}
 		}
 	}
 
--- tests/test_API.py
+++ tests/test_API.py
@@ -5,16 +5,113 @@ from api.app import app
 class TestSimple(unittest.TestCase):
 	# -- Setup --
 	def setUp(self):
-		app.config["TESTING"] = True
-		app.config["DEBUG"] = False
-		self.assertEqual(app.debug, False)
+		app.testing = True
+		app.debug = False
 
 		self.app = app.test_client()
 
 	# -- Tests --
 	def test_auth(self):
 		response = self.app.get("/api/auth")
-		self.assertEquals(response.status_code, 200)
+		self.assertEqual(response.status_code, 200)
+
+	def test_generate(self):
+		"""
+		Tests for /api/models/<name>
+		This tests text generation and parameters.
+		"""
+
+		# - Fail cases
+		# length
+		response = self.app.get("/api/models/test", data={
+			"length": -1
+		})
+		self.assertEqual(response.status_code, 400)
+
+		# temperature
+		response = self.app.get("/api/models/test", data={
+			"temperature": -1
+		})
+		self.assertEqual(response.status_code, 400)
+		response = self.app.get("/api/models/test", data={
+			"temperature": 2.3
+		})
+		self.assertEqual(response.status_code, 400)
+
+		# top_k
+		response = self.app.get("/api/models/test", data={
+			"top_k": -1
+		})
+		self.assertEqual(response.status_code, 400)
+
+		# top_p
+		response = self.app.get("/api/models/test", data={
+			"top_p": -1
+		})
+		self.assertEqual(response.status_code, 400)
+		response = self.app.get("/api/models/test", data={
+			"top_p": 3.7
+		})
+		self.assertEqual(response.status_code, 400)
+
+		# n_samples
+		response = self.app.get("/api/models/test", data={
+			"n_samples": -1
+		})
+		self.assertEqual(response.status_code, 400)
+
+		# batch_size
+		response = self.app.get("/api/models/test", data={
+			"n_samples": 1,
+			"batch_size": 2
+		})
+		self.assertEqual(response.status_code, 400)
+
+		# - Success cases
+		# default
+		response = self.app.get("/api/models/test")
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.json["meta"]["request"], {
+            "batch_size": 1,
+            "include_prefix": True,
+            "length": 512,
+            "n_samples": 1,
+            "prefix": None,
+            "sample_delimiter": "",
+            "seed": None,
+            "temperature": 0.7,
+            "top_k": 0,
+            "top_p": 0.0,
+            "truncate": None
+		})
+
+		# configured
+		response = self.app.get("/api/models/test", data={
+			"length": 4,
+			"temperature": 1.0,
+			"top_k": 1,
+			"n_samples": 2,
+			"batch_size": 2,
+			"truncate": "someword",
+			"prefix": "yeap",
+			"seed": 34895738945,
+			"include_prefix": False
+		})
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.json["meta"]["request"], {
+            "batch_size": 2,
+            "include_prefix": False,
+            "length": 4,
+            "n_samples": 2,
+            "prefix": "yeap",
+            "sample_delimiter": "====================\n",
+            "seed": 34895738945,
+            "temperature": 1.0,
+            "top_k": 1,
+            "top_p": 0.0,
+            "truncate": "someword"
+		})
+
 
 	def test_ping(self):
 		response = self.app.get("/api/ping")
