dff9013b56fc061bdf7f9ed775e6579c1b3e5833
Wes <averagewizard13@gmail.com>
2021-06-13 22:37:27 -0600

    Make Flask app use Odin interface

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
+import os
+import time, datetime
 import flask
 from flask import request, jsonify
 
 from .blueprints.jinja_endpoint import blueprint as jinja_template_blueprint
 
-import os
+import odin
 
 app = flask.Flask(__name__)
 app.register_blueprint(jinja_template_blueprint)
@@ -73,19 +75,39 @@ def models():
 	Returns all available GPT-2 models.
 	"""
 
-	return []
+	models = odin.models()
+
+	return {
+		"data": models,
+		"meta": {
+			"result_count": len(models)
+		}
+	}
 
 @app.route("/api/models/<string:name>", methods=["GET"])
-def generate(name, length):
+def generate(name):
 	"""
 	Generates text via a specified GPT-2 model.
 	"""
 
+	# TODO: consider having generate build out a dynamically generated URL to confirm generation request by requesting to generated URL
+	# Two separate endpoints: request_generate and generate. The first would return a token from the server, which would have to be sent
+	# in the call to the second endpoint.
+
 	# length = request.args.get("length")
 
-	# print(name, length)
+	start_time = datetime.datetime.now()
 
-	return {}
+	result = odin.generate(name)
+
+	gen_time = datetime.datetime.now() - start_time
+
+	return {
+		"data": result,
+		"meta": {
+			"generation_time": round(gen_time.total_seconds() * 1000)
+		}
+	}
 
 
 if __name__ == "__main__":
