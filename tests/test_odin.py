e1033a464d297efa48423320b3bc22b10fe653d7
Wes <averagewizard13@gmail.com>
2021-06-17 00:14:54 -0600

    Add ODIN unit test skeleton

---
+++ tests/test_odin.py
@@ -0,0 +1,15 @@
+import unittest
+
+import odin
+
+class TestOdin(unittest.TestCase):
+	# -- Setup --
+	def setUp(self):
+		pass
+
+	# -- Tests --
+	def test_list_models(self):
+		models = odin.models()
+
+	def test_generate(self):
+		pass
