b80674c797a65cf074694dade6760111c43831b2
Wes <averagewizard13@gmail.com>
2021-06-27 21:39:11 -0600

    Fix unit tests

--- tests/test_API.py
+++ tests/test_API.py
@@ -20,7 +20,7 @@ class TestAPI(unittest.TestCase):
 			"username": os.getenv("USERNAME"),
 			"password": os.getenv("PASSWORD")
 		}
-		response = self.app.get("/api/auth", data=data)
+		response = self.app.post("/api/auth", json=data)
 		self.assertEqual(response.status_code, 200)
 
 	def test_generate(self):
@@ -33,11 +33,12 @@ class TestAPI(unittest.TestCase):
 
 		endpoint = "/api/models/test"
 
+		# Log in
 		data = {
 			"username": os.getenv("USERNAME"),
 			"password": os.getenv("PASSWORD")
 		}
-		token = self.app.get("/api/auth", data=data).get_data(as_text=True)
+		token = self.app.post("/api/auth", json=data).get_data(as_text=True)
 
 		headers = {
 			"Authorization": f"Bearer {token}"
@@ -45,56 +46,56 @@ class TestAPI(unittest.TestCase):
 
 		# - Fail cases
 		# length
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"length": -1
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
 
 		# seed
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"seed": 2**33
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"seed": 48573,
 			"n_samples": 2
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
 
 		# temperature
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"temperature": -1
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"temperature": 2.3
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
 
 		# top_k
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"top_k": -1
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
 
 		# top_p
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"top_p": -1
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"top_p": 3.7
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
 
 		# n_samples
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"n_samples": -1
 		}, headers=headers)
 		self.assertEqual(response.status_code, 400)
 
 		# batch_size
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"n_samples": 1,
 			"batch_size": 2
 		}, headers=headers)
@@ -102,23 +103,23 @@ class TestAPI(unittest.TestCase):
 
 		# - Success cases
 		# default
-		response = self.app.get(endpoint, headers=headers)
+		response = self.app.post(endpoint, json={}, headers=headers)
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(response.json["meta"]["parameters"], {
-            "batch_size": defaults.BATCH_SIZE,
-            "include_prefix": defaults.INCLUDE_PREFIX,
-            "length": defaults.LENGTH,
-            "n_samples": defaults.N_SAMPLES,
-            "prefix": defaults.PREFIX,
-            "seed": defaults.SEED,
-            "temperature": defaults.TEMPERATURE,
-            "top_k": defaults.TOP_K,
-            "top_p": defaults.TOP_P,
-            "truncate": defaults.TRUNCATE
+			"batch_size": defaults.BATCH_SIZE,
+			"include_prefix": defaults.INCLUDE_PREFIX,
+			"length": defaults.LENGTH,
+			"n_samples": defaults.N_SAMPLES,
+			"prefix": defaults.PREFIX,
+			"seed": defaults.SEED,
+			"temperature": defaults.TEMPERATURE,
+			"top_k": defaults.TOP_K,
+			"top_p": defaults.TOP_P,
+			"truncate": defaults.TRUNCATE
 		})
 
 		# configured
-		response = self.app.get(endpoint, data={
+		response = self.app.post(endpoint, json={
 			"length": 4,
 			"temperature": 1.0,
 			"top_k": 1,
@@ -130,16 +131,16 @@ class TestAPI(unittest.TestCase):
 		}, headers=headers)
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(response.json["meta"]["parameters"], {
-            "batch_size": 2,
-            "include_prefix": False,
-            "length": 4,
-            "n_samples": 2,
-            "prefix": "yeap",
-            "seed": None,
-            "temperature": 1.0,
-            "top_k": 1,
-            "top_p": 0.0,
-            "truncate": "someword"
+			"batch_size": 2,
+			"include_prefix": False,
+			"length": 4,
+			"n_samples": 2,
+			"prefix": "yeap",
+			"seed": None,
+			"temperature": 1.0,
+			"top_k": 1,
+			"top_p": 0.0,
+			"truncate": "someword"
 		})
 
 
